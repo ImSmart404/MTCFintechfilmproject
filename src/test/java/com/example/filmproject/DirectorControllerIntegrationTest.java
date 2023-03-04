10dbd0f74831dc8c12f902745a1685e3f184d72b
ImSmart404 <71227837+ImSmart404@users.noreply.github.com>
2023-03-04 23:15:17 +0300

    dz16_Mikhaylov

---
+++ src/test/java/com/example/filmproject/DirectorControllerIntegrationTest.java
@@ -0,0 +1,62 @@
+package com.example.filmproject;
+import com.example.filmproject.controller.DirectorController;
+import com.example.filmproject.model.Director;
+import com.example.filmproject.service.DirectorService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import java.util.UUID;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(DirectorController.class)
+public class DirectorControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private DirectorService directorService;
+
+    @Test
+    public void testSaveDirector() throws Exception {
+        Director director = new Director();
+        director.setUuid(UUID.randomUUID());
+        director.setName("Mikhail");
+        director.setAge(21);
+        director.setCountry("Russia");
+        when(directorService.saveDirector(any(Director.class))).thenReturn(director);
+        mockMvc.perform(post("/director")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"name\": \"Mikhail\", \"age\": 21, \"country\": \"Russia\"}"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.name", equalTo("Mikhail")))
+                .andExpect(jsonPath("$.age", equalTo(21)))
+                .andExpect(jsonPath("$.country", equalTo("Russia")));
+    }
+    @Test
+    public void testGetDirector() throws Exception {
+        UUID uuid = UUID.randomUUID();
+        Director director = new Director();
+        director.setUuid(uuid);
+        director.setName("Mikhail");
+        director.setAge(21);
+        director.setCountry("Russia");
+        when(directorService.getDirector(uuid)).thenReturn(director);
+        mockMvc.perform(get("/director")
+                        .param("directorUuid", uuid.toString()))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.name", equalTo("Mikhail")))
+                .andExpect(jsonPath("$.age", equalTo(21)))
+                .andExpect(jsonPath("$.country", equalTo("Russia")));
+    }
+
+}
---
+++ src/test/java/com/example/filmproject/FilmControllerIntegrationTest.java
@@ -0,0 +1,63 @@
+package com.example.filmproject;
+import com.example.filmproject.constants.GenreEnum;
+import com.example.filmproject.controller.FilmController;
+import com.example.filmproject.model.Film;
+import com.example.filmproject.service.FilmService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import java.util.UUID;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(FilmController.class)
+public class FilmControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private FilmService filmService;
+
+    @Test
+    public void testSaveFilm() throws Exception {
+        Film film = new Film();
+        film.setUuid(UUID.randomUUID());
+        film.setTitle("Rambo");
+        film.setGenre(GenreEnum.ACTION);
+        film.setRating(10);
+        when(filmService.saveFilm(any(Film.class))).thenReturn(film);
+        mockMvc.perform(post("/film")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"title\": \"Rambo\", \"genre\": \"ACTION\", \"rating\": 10}"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.title", equalTo("Rambo")))
+                .andExpect(jsonPath("$.genre", equalTo("ACTION")))
+                .andExpect(jsonPath("$.rating", equalTo(10)));
+    }
+
+    @Test
+    public void testGetFilm() throws Exception {
+        UUID uuid = UUID.randomUUID();
+        Film film = new Film();
+        film.setUuid(uuid);
+        film.setTitle("Rambo");
+        film.setGenre(GenreEnum.ACTION);
+        film.setRating(10);
+        when(filmService.getFilm(uuid)).thenReturn(film);
+        mockMvc.perform(get("/film")
+                        .param("filmUuid", uuid.toString()))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.title", equalTo("Rambo")))
+                .andExpect(jsonPath("$.genre", equalTo("ACTION")))
+                .andExpect(jsonPath("$.rating", equalTo(10)));
+    }
+}
